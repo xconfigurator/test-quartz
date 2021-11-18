e7e9016f0de969fecc2782b9ca66d677f60a9584
xconfigurator <xconfigurator@hotmail.com>
2021-11-18 11:23:33 +0800

    使用Spring推荐的写法

--- src/main/java/liuyang/testquartz/modules/scheduler/quartz/quartzjobbean/FooJob.java
+++ src/main/java/liuyang/testquartz/modules/scheduler/quartz/quartzjobbean/FooJob.java
@@ -17,29 +17,32 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @DisallowConcurrentExecution
 @PersistJobDataAfterExecution
-//public class FooJob extends QuartzJobBean {
-public class FooJob implements Job {// 注意：虽然可以这样做，但还是推荐使用Spring Boot文档中推荐的写法 extends QuartzJobBean
+public class FooJob extends QuartzJobBean {
+//public class FooJob implements Job {// 注意：虽然可以这样做，但还是推荐使用Spring Boot文档中推荐的写法 extends QuartzJobBean
 
     @Autowired // Spring自己的示例工程也是这样注入的。虽然Spring建议是通过构造方法注入。
     private DemoService demoService;// 在Job中享受IoC（实测，使用implements Job也可享受Spring提供的IoC服务, 虽然）
 
     // QuartzJobBean // 推荐用这种写法
-    //@Override
+    @Override
     protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
-        log.info("bar QuartzJobBean... FooJob被 {} 触发执行了！ ", context.getTrigger().getKey());
-        log.info("bar QuartzJobBean... demoService = " + demoService);
-        log.info("bar QuartzJobBean... demoService.hey() = {}", demoService.hey() );
+        log.info("xx bar Job... FooJob 被TritterKey = {} 的触发器触发了！ ", context.getTrigger().getKey());
+        /*
+        log.info("bar Job... IoC测试 demoService = " + demoService);
+        log.info("bar Job... IoC测试 demoService.hey() = {}", demoService.hey() );
+        log.info("bar Job... Param测试 JobDataMap = {}", JSON.toJSONString(context.getJobDetail().getJobDataMap()));
+        */
     }
 
     // Job // 只是验证一下能不能这样搞
+    /*
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
         log.info("bar Job... FooJob 被TritterKey = {} 的触发器触发了！ ", jobExecutionContext.getTrigger().getKey());
-        /*
         log.info("bar Job... FooJob JobKey = {}", jobExecutionContext.getJobDetail().getKey());
         log.info("bar Job... IoC测试 demoService = " + demoService);
         log.info("bar Job... IoC测试 demoService.hey() = {}", demoService.hey() );
         log.info("bar Job... Param测试 JobDataMap = {}", JSON.toJSONString(jobExecutionContext.getJobDetail().getJobDataMap()));
-        */
     }
+    */
 }
